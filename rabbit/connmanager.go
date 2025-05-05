c352768ae6ca63560dcba9e4e4b7f6a26b6ad905
Pavel Krush <neon@pushwoosh.com>
2025-05-05 20:27:30 +0700

    review

--- rabbit/connmanager.go
+++ rabbit/connmanager.go
@@ -7,19 +7,21 @@ import (
 	"sync/atomic"
 	"time"
 
+	infralog "github.com/pushwoosh/infra/log"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"go.uber.org/zap"
 )
 
 var hostname = os.Getenv("HOSTNAME")
 
-type connItem struct {
+type connection struct {
 	cfg      *ConnectionConfig
 	amqpConn *amqp.Connection
-	channels map[*channelItem]bool
+	channels map[*channel]bool
 	isDead   atomic.Bool
 }
 
-func (ci *connItem) MarkAsDead() {
+func (ci *connection) MarkAsDead() {
 	if ci == nil {
 		return
 	}
@@ -27,7 +29,7 @@ func (ci *connItem) MarkAsDead() {
 	ci.isDead.Store(true)
 }
 
-type channelItem struct {
+type channel struct {
 	cfg                *ConsumerConfig
 	messagesInProgress sync.WaitGroup
 	amqpChannel        *amqp.Channel
@@ -35,256 +37,47 @@ type channelItem struct {
 	isDead             atomic.Bool
 }
 
-func (ci *channelItem) InProgressIncrement() {
-	ci.messagesInProgress.Add(1)
+func (ch *channel) InProgressIncrement() {
+	ch.messagesInProgress.Add(1)
 }
 
-func (ci *channelItem) InProgressDecrement() {
-	ci.messagesInProgress.Done()
+func (ch *channel) InProgressDecrement() {
+	ch.messagesInProgress.Done()
 }
 
-func (ci *channelItem) MarkAsDead() {
-	if ci == nil {
+// MarkAsDead marks the channel as dead. ci can be nil.
+func (ch *channel) MarkAsDead() {
+	if ch == nil {
 		return
 	}
 
-	ci.isDead.Store(true)
+	ch.isDead.Store(true)
 }
 
-func (ci *channelItem) errCallback(err error) {
-	if ci == nil || ci.cfg == nil || ci.cfg.ErrCallback == nil || err == nil {
+func (ch *channel) errCallback(err error) {
+	if ch == nil || ch.cfg == nil || ch.cfg.ErrCallback == nil || err == nil {
 		return
 	}
 
-	ci.cfg.ErrCallback(err)
+	ch.cfg.ErrCallback(err)
 }
 
-type connManager struct {
-	mu    sync.Mutex
-	conns map[*connItem]string
-}
-
-func newConnManager() *connManager {
-	cm := &connManager{
-		conns: make(map[*connItem]string),
-	}
-
-	go cm.collectMetrics()
-	go cm.cleanDeadConnections()
-
-	return cm
-}
-
-func (cm *connManager) GetChannel(connCfg *ConnectionConfig, consumerCfg *ConsumerConfig) (*connItem, *channelItem, error) {
-	cm.mu.Lock()
-	defer cm.mu.Unlock()
-
-	connectionItem, err := cm.createConnection(connCfg, consumerCfg.Tag)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	chItem, err := cm.createChannel(connectionItem, consumerCfg)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	connectionItem.channels[chItem] = true
-	return connectionItem, chItem, nil
-}
-
-func (cm *connManager) createChannel(connItem *connItem, consumerCfg *ConsumerConfig) (*channelItem, error) {
-	channel, err := connItem.amqpConn.Channel()
-	if err != nil {
-		return nil, err
-	}
-
-	prefetchCount := consumerCfg.PrefetchCount
-	if prefetchCount < 1 {
-		prefetchCount = defaultPrefetchCount
-	}
-
-	// very important to set prefetch count,
-	// or you may get memory leak!
-	if err = channel.Qos(prefetchCount, 0, false); err != nil {
-		return nil, err
-	}
-
-	queuePriority := consumerCfg.QueuePriority
-	args := amqp.Table{}
-	if queuePriority > 0 {
-		args[PriorityProperty] = int(queuePriority)
-	}
-
-	_, err = channel.QueueDeclare(
-		consumerCfg.Queue, // name of the queue
-		false,             // durable
-		false,             // delete when unused
-		false,             // exclusive
-		false,             // noWait
-		args,              // arguments
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	chItem := &channelItem{
-		cfg:         consumerCfg,
-		amqpChannel: channel,
-	}
-
-	cm.handleChannelErrors(chItem)
-
-	deliveries, err := channel.Consume(
-		consumerCfg.Queue, // queue name
-		consumerCfg.Tag,   // consumerTag,
-		false,             // autoAck
-		false,             // exclusive
-		false,             // noLocal
-		false,             // noWait
-		nil,               // arguments
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	chItem.deliveries = deliveries
-	connItem.channels[chItem] = true
-	return chItem, nil
-}
-
-func (cm *connManager) createConnection(cfg *ConnectionConfig, tag string) (*connItem, error) {
-	amqpURL := createAMQPURL(cfg)
-	for c, url := range cm.conns {
-		if url == amqpURL && !c.isDead.Load() {
-			return c, nil
-		}
-	}
-
-	amqpProps := amqp.NewConnectionProperties()
-	if tag == "" {
-		tag = hostname
-	}
-	amqpProps.SetClientConnectionName(tag)
-
-	conn, err := amqp.DialConfig(amqpURL, amqp.Config{
-		Properties: amqpProps,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	cItem := &connItem{
-		cfg:      cfg,
-		amqpConn: conn,
-		channels: make(map[*channelItem]bool),
-	}
-
-	cm.handleConnErrors(cItem)
-	cm.conns[cItem] = amqpURL
-	return cItem, nil
-}
-
-func (cm *connManager) handleChannelErrors(chItem *channelItem) {
-	go func() {
-		errorsCh := chItem.amqpChannel.NotifyClose(make(chan *amqp.Error))
-		for err := range errorsCh {
-			if err != nil {
-				chItem.MarkAsDead()
-				chItem.errCallback(err)
-			}
-		}
-	}()
-}
-
-func (cm *connManager) handleConnErrors(cItem *connItem) {
-	go func() {
-		errorsCh := cItem.amqpConn.NotifyClose(make(chan *amqp.Error))
-		for connErr := range errorsCh {
-			if connErr != nil { // where to put errors?
-				cItem.MarkAsDead()
-			}
-		}
-	}()
-}
-
-func (cm *connManager) collectMetrics() {
-	ticker := time.NewTicker(time.Minute / 2)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		cm.mu.Lock()
-		for conn := range cm.conns {
-			if conn.isDead.Load() {
-				continue
-			}
-
-			for channel := range conn.channels {
-				if channel.isDead.Load() {
-					continue
-				}
-
-				go collectMetrics(conn, channel)
-			}
-		}
-		cm.mu.Unlock()
-	}
-}
-
-func (cm *connManager) cleanDeadConnections() {
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		cm.mu.Lock()
-		for conn := range cm.conns {
-			if conn.isDead.Load() && len(conn.channels) == 0 {
-				delete(cm.conns, conn)
-				go cm.closeDeadConnection(conn)
-				continue
-			}
-
-			for channel := range conn.channels {
-				if !channel.isDead.Load() && !conn.isDead.Load() {
-					continue
-				}
-
-				delete(conn.channels, channel)
-				go cm.closeDeadChannel(channel)
-			}
-		}
-		cm.mu.Unlock()
-	}
-}
-
-func (cm *connManager) closeDeadConnection(conn *connItem) {
-	_ = conn.amqpConn.Close() // where to put errors?
-}
-
-func (cm *connManager) closeDeadChannel(channel *channelItem) {
-	channel.messagesInProgress.Wait()
-	err := channel.amqpChannel.Close()
-
-	channel.errCallback(err)
-}
-
-func collectMetrics(connItem *connItem, chItem *channelItem) {
+func (ch *channel) collectMetrics(host string) {
 	defer func() {
 		if e := recover(); e != nil {
-			chItem.errCallback(fmt.Errorf("%v", e))
+			ch.errCallback(fmt.Errorf("%v", e))
 			return
 		}
 	}()
 
-	queue := chItem.cfg.Queue
-	metrics := chItem.cfg.Metrics
-	host, _ := getHostPort(connItem.cfg.Address)
+	queue := ch.cfg.Queue
+	metrics := ch.cfg.Metrics
 
-	if chItem.amqpChannel.IsClosed() || metrics == nil {
+	if ch.amqpChannel.IsClosed() || metrics == nil {
 		return
 	}
 
-	q, err := chItem.amqpChannel.QueueDeclarePassive(
+	q, err := ch.amqpChannel.QueueDeclarePassive(
 		queue,
 		false, // durable
 		false, // delete when unused
@@ -309,7 +102,7 @@ func collectMetrics(connItem *connItem, chItem *channelItem) {
 		return
 	}
 
-	msg, ok, err := chItem.amqpChannel.Get(queue, false)
+	msg, ok, err := ch.amqpChannel.Get(queue, false)
 	if err == nil && ok {
 		seconds := time.Since(msg.Timestamp).Seconds()
 		_ = msg.Reject(true)
@@ -317,4 +110,215 @@ func collectMetrics(connItem *connItem, chItem *channelItem) {
 			metrics.QueueDelay(host, queue, int64(seconds))
 		}
 	}
+}
+
+type connManager struct {
+	mu    sync.Mutex
+	conns map[*connection]string
+}
+
+func newConnManager() *connManager {
+	cm := &connManager{
+		conns: make(map[*connection]string),
+	}
+
+	go cm.collectMetrics()
+	go cm.cleanDeadConnections()
+
+	return cm
+}
+
+func (cm *connManager) GetChannel(connCfg *ConnectionConfig, consumerCfg *ConsumerConfig) (*connection, *channel, error) {
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
+
+	conn, err := cm.createConnection(connCfg, consumerCfg.Tag)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ch, err := cm.createChannel(conn, consumerCfg)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	conn.channels[ch] = true
+	return conn, ch, nil
+}
+
+func (cm *connManager) createChannel(connItem *connection, consumerCfg *ConsumerConfig) (*channel, error) {
+	ch, err := connItem.amqpConn.Channel()
+	if err != nil {
+		return nil, err
+	}
+
+	prefetchCount := consumerCfg.PrefetchCount
+	if prefetchCount < 1 {
+		prefetchCount = defaultPrefetchCount
+	}
+
+	// very important to set prefetch count,
+	// or you may get memory leak!
+	if err = ch.Qos(prefetchCount, 0, false); err != nil {
+		return nil, err
+	}
+
+	queuePriority := consumerCfg.QueuePriority
+	args := amqp.Table{}
+	if queuePriority > 0 {
+		args[PriorityProperty] = int(queuePriority)
+	}
+
+	_, err = ch.QueueDeclare(
+		consumerCfg.Queue, // name of the queue
+		false,             // durable
+		false,             // delete when unused
+		false,             // exclusive
+		false,             // noWait
+		args,              // arguments
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	chItem := &channel{
+		cfg:         consumerCfg,
+		amqpChannel: ch,
+	}
+
+	cm.handleChannelErrors(chItem)
+
+	deliveries, err := ch.Consume(
+		consumerCfg.Queue, // queue name
+		consumerCfg.Tag,   // consumerTag,
+		false,             // autoAck
+		false,             // exclusive
+		false,             // noLocal
+		false,             // noWait
+		nil,               // arguments
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	chItem.deliveries = deliveries
+	connItem.channels[chItem] = true
+	return chItem, nil
+}
+
+func (cm *connManager) createConnection(cfg *ConnectionConfig, tag string) (*connection, error) {
+	amqpURL := createAMQPURL(cfg)
+	for c, url := range cm.conns {
+		if url == amqpURL && !c.isDead.Load() {
+			return c, nil
+		}
+	}
+
+	amqpProps := amqp.NewConnectionProperties()
+	if tag == "" {
+		tag = hostname
+	}
+	amqpProps.SetClientConnectionName(tag)
+
+	conn, err := amqp.DialConfig(amqpURL, amqp.Config{
+		Properties: amqpProps,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	cItem := &connection{
+		cfg:      cfg,
+		amqpConn: conn,
+		channels: make(map[*channel]bool),
+	}
+
+	cm.handleConnErrors(cItem)
+	cm.conns[cItem] = amqpURL
+	return cItem, nil
+}
+
+func (cm *connManager) handleChannelErrors(chItem *channel) {
+	go func() {
+		errorsCh := chItem.amqpChannel.NotifyClose(make(chan *amqp.Error))
+		for err := range errorsCh {
+			if err != nil {
+				chItem.MarkAsDead()
+				chItem.errCallback(err)
+			}
+		}
+	}()
+}
+
+func (cm *connManager) handleConnErrors(cItem *connection) {
+	go func() {
+		errorsCh := cItem.amqpConn.NotifyClose(make(chan *amqp.Error))
+		for connErr := range errorsCh {
+			if connErr != nil { // where to put errors?
+				cItem.MarkAsDead()
+				infralog.Error("error on rabbitmq connection", zap.Error(connErr))
+			}
+		}
+	}()
+}
+
+func (cm *connManager) collectMetrics() {
+	ticker := time.NewTicker(time.Minute / 2)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		cm.mu.Lock()
+		for conn := range cm.conns {
+			if conn.isDead.Load() {
+				continue
+			}
+
+			host, _ := getHostPort(conn.cfg.Address)
+			for ch := range conn.channels {
+				if ch.isDead.Load() {
+					continue
+				}
+
+				go ch.collectMetrics(host)
+			}
+		}
+		cm.mu.Unlock()
+	}
+}
+
+func (cm *connManager) cleanDeadConnections() {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		cm.mu.Lock()
+		for conn := range cm.conns {
+			if conn.isDead.Load() && len(conn.channels) == 0 {
+				delete(cm.conns, conn)
+				go cm.closeDeadConnection(conn)
+				continue
+			}
+
+			for ch := range conn.channels {
+				if !ch.isDead.Load() && !conn.isDead.Load() {
+					continue
+				}
+
+				delete(conn.channels, ch)
+				go cm.closeDeadChannel(ch)
+			}
+		}
+		cm.mu.Unlock()
+	}
+}
+
+func (cm *connManager) closeDeadConnection(conn *connection) {
+	_ = conn.amqpConn.Close() // where to put errors?
+}
+
+func (cm *connManager) closeDeadChannel(channel *channel) {
+	channel.messagesInProgress.Wait()
+	err := channel.amqpChannel.Close()
+
+	channel.errCallback(err)
 }
